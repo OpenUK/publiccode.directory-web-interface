627b907162ac0b909d16519f6b501f0f6a319a15
JohnMica <liviumica@me.com>
2019-01-23 10:07:38 +0000

    eslint restyling following better rules

--- themes/public-code-directory/resources/js/app.js
+++ themes/public-code-directory/resources/js/app.js
@@ -1,27 +1,27 @@
 import "babel-polyfill";
 import store from "./store";
-import Buefy from 'buefy'
+import Buefy from "buefy";
 
-window.Vue = require('vue');
+window.Vue = require("vue");
 
-Vue.filter("capitalize", function (value) {
-    if (!value) return "";
-    value = value.toString();
-    return value.charAt(0).toUpperCase() + value.slice(1);
+Vue.filter("capitalize", function(value) {
+  if (!value) return "";
+  value = value.toString();
+  return value.charAt(0).toUpperCase() + value.slice(1);
 });
-Vue.filter("truncate", function (text, stop, clamp) {
-    return text.slice(0, stop) + (stop < text.length ? clamp || "..." : "");
+Vue.filter("truncate", function(text, stop, clamp) {
+  return text.slice(0, stop) + (stop < text.length ? clamp || "..." : "");
 });
 
-Vue.use(Buefy)
-Vue.component('navbar', require('./components/Navbar.vue'));
-Vue.component('products', require('./components/Products.vue'));
-Vue.component('worldmap', require('./components/Worldmap.vue'));
+Vue.use(Buefy);
+Vue.component("navbar", require("./components/Navbar.vue"));
+Vue.component("products", require("./components/Products.vue"));
+Vue.component("worldmap", require("./components/Worldmap.vue"));
 
 Vue.config.productionTip = false;
 
 new Vue({
-    el: '#app',
-    store,
-    delimiters: ['${', '}']
+  el: "#app",
+  store,
+  delimiters: ["${", "}"]
 });
